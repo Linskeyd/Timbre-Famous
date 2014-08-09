0535d890803080e570845643e48b4e956304ace4
Dave Linskey <linskeyd@gmail.com>
2014-08-09 13:58:23 -0400

    Added in a skewed StripView inside of a MenuView inside of the AppView, simply behind all the other content and revealed when top level PageView surface is moved

--- src/views/StripView.js
+++ src/views/StripView.js
@@ -6,12 +6,42 @@ define(function(require, exports, module) {
 
     function StripView() {
         View.apply(this, arguments);
+
+        _createBackground.call(this);
     }
 
     StripView.prototype = Object.create(View.prototype);
     StripView.prototype.constructor = StripView;
 
-    StripView.DEFAULT_OPTIONS = {};
+    StripView.DEFAULT_OPTIONS = {
+        width: 320,
+        height: 55,
+        angle: -0.2
+    };
+
+    function _createBackground() {
+        var backgroundSurface = new Surface({
+            size: [this.options.width, this.options.height],
+            properties: {
+                backgroundColor: 'black',
+                // on certain devices, a skewed surface can have jagged edges
+                // 1px box-shadow provides anti-aliasing to soften the jaggedness
+                // just like when anti-alias is used to pull a texture from an atlas
+                boxShadow: '0 0 1px rgba(0, 0, 0, 1)'
+            }
+        });
+
+        var rotateModifier = new StateModifier({
+            transform: Transform.rotateZ(this.options.angle)
+        });
+
+        var skewModifier = new StateModifier({
+            transform: Transform.skew(0, 0, this.options.angle)
+        });
+
+        // we're first skewing our surface then rotating it
+        this.add(rotateModifier).add(skewModifier).add(backgroundSurface);
+    }
 
     module.exports = StripView;
 });
